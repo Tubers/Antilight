334415fd0cb119ecf5ccf930eccd5fc8b477a86a
lgeorge4 <lgeorge4@villanova.edu>
2016-04-27 22:06:35 -0400

    Skeletal References

---
+++ src/main/java/com/tubers/antilight/References.java
@@ -0,0 +1,32 @@
+package com.tubers.antilight;
+
+
+import java.util.Locale;
+
+public class References {
+
+    public static class Mod
+    {
+        public static final String MODID = "antilight";
+        public static final String DOMAIN = MODID.toLowerCase(Locale.ENGLISH) + ":";
+        public static final String NAME = "Antilight: Darkness Evolved";
+        public static final String VERSION = "1.0";
+    }
+
+    public static final class Gui
+    {
+
+    }
+
+    public enum AntilightItem
+    {
+
+    }
+
+    public enum AntilightBlock
+    {
+
+    }
+
+
+}
